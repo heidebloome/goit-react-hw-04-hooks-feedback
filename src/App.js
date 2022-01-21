066f75001adbc01ec265ddd0dad781e876d97c3a
Vira Velmozhna <viravelmozhna@gmail.com>
2022-01-21 10:32:00 +0200

    refactoring App to functional component with using hooks

--- src/App.js
+++ src/App.js
@@ -1,58 +1,109 @@
-import React, { Component, Fragment } from 'react';
+import { useState } from 'react';
 
 import Section from './components/Section/Section';
 import FeedbackOptions from './components/FeedbackOptions/FeedbackOptions';
 import Statistics from './components/Statistics/Statistics';
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const updateFeedbackCount = option => {
+    switch (option.toLowerCase()) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      default:
+        return;
+    }
   };
 
-  updateFeedbackCount = option => {
-    this.setState(prevState => {
-      const feedbackOption = option.toLowerCase();
-      return {
-        [feedbackOption]: prevState[feedbackOption] + 1,
-      };
-    });
-  };
-
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  const countTotalFeedback = () => {
     return good + neutral + bad;
   };
 
-  countPositiveFeedbackPercentage = () => {
-    return Math.round((this.state.good * 100) / this.countTotalFeedback());
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((good * 100) / countTotalFeedback());
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={['Good', 'Neutral', 'Bad']}
+          onLeaveFeedback={updateFeedbackCount}
+        />
+      </Section>
 
-    return (
-      <Fragment>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={['Good', 'Neutral', 'Bad']}
-            onLeaveFeedback={this.updateFeedbackCount}
-          />
-        </Section>
+      <Section title="Statistics">
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          totalFeedbackCount={countTotalFeedback}
+          positiveFeedbackCount={countPositiveFeedbackPercentage}
+        />
+      </Section>
+    </>
+  );
+};
 
-        <Section title="Statistics">
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            totalFeedbackCount={this.countTotalFeedback}
-            positiveFeedbackCount={this.countPositiveFeedbackPercentage}
-          />
-        </Section>
-      </Fragment>
-    );
-  }
-}
+// class App extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+
+//   updateFeedbackCount = option => {
+//     this.setState(prevState => {
+//       const feedbackOption = option.toLowerCase();
+//       return {
+//         [feedbackOption]: prevState[feedbackOption] + 1,
+//       };
+//     });
+//   };
+
+//   countTotalFeedback = () => {
+//     const { good, neutral, bad } = this.state;
+//     return good + neutral + bad;
+//   };
+
+//   countPositiveFeedbackPercentage = () => {
+//     return Math.round((this.state.good * 100) / this.countTotalFeedback());
+//   };
+
+//   render() {
+//     const { good, neutral, bad } = this.state;
+
+//     return (
+//       <>
+//         <Section title="Please leave feedback">
+//           <FeedbackOptions
+//             options={['Good', 'Neutral', 'Bad']}
+//             onLeaveFeedback={this.updateFeedbackCount}
+//           />
+//         </Section>
+
+//         <Section title="Statistics">
+//           <Statistics
+//             good={good}
+//             neutral={neutral}
+//             bad={bad}
+//             totalFeedbackCount={this.countTotalFeedback}
+//             positiveFeedbackCount={this.countPositiveFeedbackPercentage}
+//           />
+//         </Section>
+//       </>
+//     );
+//   }
+// }
 
 export default App;
